0f31ad60a43def759eefe0c245ed9543810d03c6
mille-f <f11235tetra@gmail.com>
2016-03-17 03:37:50 +0900

    [modify] alkali2.rb

--- src/alkali2.rb
+++ src/alkali2.rb
@@ -2,7 +2,7 @@ require 'pg'
 require 'natto'
 
 @connect = PG::connect(dbname: 'test1')
-@natto = Natto::MeCab.new(userdic: "/home/vagrant/Aqua/dic/alkali.dic")
+@natto = Natto::MeCab.new(userdic: "/home/vagrant/aquarium/dic/alkali.dic")
 
 puts "問題を作成してください。(例:XのYは何ですか？)"
 print "=> "
@@ -10,7 +10,7 @@ txt = gets.chomp
 
 noun = Array.new
 @natto.parse(txt) do |n|
-  if n.feature.split(',')[0] == '名詞' then
+  if n.feature.split(',')[0] == '名詞' || n.feature.split(',')[0] == '動詞' then
     noun.push(n.surface)
   end
 end
@@ -27,7 +27,7 @@ print "誤答3=> "
 w3 = gets.chomp
 
 def search(e, a, v)
-  @connect.exec("select * from alkali where entity like '#{e}' AND attribute like '#{a}' AND value like '#{v}'")
+    @connect.exec("select * from alkali where entity like '#{e}' AND attribute like '#{a}' AND value like '#{v}'")
 end
 
 res1 = search(e, a, v)
@@ -36,22 +36,22 @@ res3 = search(e, a, w2)
 res4 = search(e, a, w3)
 
 def right(e, a)
-  @connect.exec("select value from alkali where entity like '#{e}' AND attribute like '#{a}' AND value like '%'")[0].to_h.fetch("value")
+    @connect.exec("select value from alkali where entity like '#{e}' AND attribute like '#{a}' AND value like '%'")[0].to_h.fetch("value")
 end
 
 puts ""
 if res1.count == 0 then
-  puts "正答が間違っています。"
-  puts "正しくは、#{e}の#{a}は#{right(e, a)}です。"
+    puts "正答が間違っています。"
+    puts "正しくは、#{e}の#{a}は#{right(e, a)}です。"
 elsif res2.count >= 1 then
-  puts "誤答1が間違っています。"
-  puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
+    puts "誤答1が間違っています。"
+    puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
 elsif res3.count >= 1 then
-  puts "誤答2が間違っています。"
-  puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
+    puts "誤答2が間違っています。"
+    puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
 elsif res4.count >= 1 then
-  puts "誤答3が間違っています。"
-  puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
+    puts "誤答3が間違っています。"
+    puts "#{e}の#{a}は#{right(e, a)}は正しいです。"
 else puts "良い問題ですね。" end
 
 @connect.finish
