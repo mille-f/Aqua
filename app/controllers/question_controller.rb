0eb5352b26d5679f85f5c54b300a01e27e7c402e
mille-f <f11235tetra@gmail.com>
2016-03-20 06:16:39 +0900

    [add] correctness determination

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,12 +1,25 @@
 class QuestionController < ApplicationController
   def create
+    nm = Natto::MeCab.new(userdic: "/home/vagrant/aquarium/dic/alkali.dic")
     @problem = params[:problem]
-    @right = params[:right]
-    @wrong1 = params[:wrong1]
-    @wrong2 = params[:wrong2]
-    @wrong3 = params[:wrong3]
+    word = Array.new
+    unless @problem.blank? then
+      nm.parse(@problem) do |w|
+        if w.feature.split(',')[0] == '名詞' || w.feature.split(',')[0] == '動詞' then
+          word.push(w.surface)
+        end
+      end
+    end
+
+    @e = word[0]
+    @a = word[1]
+    @v = params[:right]
+    w1 = params[:wrong1]
+    w2 = params[:wrong2]
+    w3 = params[:wrong3]
+
     con = ActiveRecord::Base.connection
-    @res1 = con.select_all('select val from alkalis').to_a
+    @res1 = con.select_all("select val from alkalis where ent like '#{@e}' and att like '#{@a}' and val like '#{@v}'").to_a
   end
 
   def list
