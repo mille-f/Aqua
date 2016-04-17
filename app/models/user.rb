8bd1b2e9bfab9089cf03f34588faab7f9822dad7
mille-f <f11235tetra@gmail.com>
2016-04-17 11:51:42 +0900

    [modify] delete email form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: true
+
+  # Email is not required
+  def email_required?
+    false
+  end
+
 end
