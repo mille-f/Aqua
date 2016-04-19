c119d91367fb7ebb1bafa6003cdd39267cfd05d1
mille-f <f11235tetra@gmail.com>
2016-04-20 02:18:41 +0900

    [add] Better Errors settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # devise settings
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  # Better_Errors settings
+  BetterErrors::Middleware.allow_ip! "192.168.33.1"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
