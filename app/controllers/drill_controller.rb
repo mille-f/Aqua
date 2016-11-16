16a0c6cf5984846ece2c3b096c9a9fc9ce386791
mille-f <f11235tetra@gmail.com>
2016-11-17 01:27:03 +0900

    [WIP] make 20 poblems

--- app/controllers/drill_controller.rb
+++ app/controllers/drill_controller.rb
@@ -1,11 +1,20 @@
 class DrillController < ApplicationController
   def drill
-    @cnt = 1
+    con = ActiveRecord::Base.connection
+    @role  = current_user.role.to_i
+    @user  = current_user.username.to_s.capitalize
+    @base  = "#{@user}Alkali".constantize
+
+    if @role == 0 then
+      @data  = @base.all
+      @size = @data.size # 知識ベースのサイズ
+      @set = (1..@size).to_a.sample(19) #20個ランダム
+    end
 
     @is_hit = false
     if params['pass'] then
       @is_hit = true
-      @cnt += 1
     end
+
   end
 end
