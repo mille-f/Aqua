685e0baa76bc895275dcebddbd07b243ea3cd2da
mille-f <f11235tetra@gmail.com>
2017-01-22 18:14:45 +0900

    [add] link

--- app/controllers/drill_controller.rb
+++ app/controllers/drill_controller.rb
@@ -1,6 +1,6 @@
 class DrillController < ApplicationController
   def drill
-    con = ActiveRecord::Base.connection
+    #con = ActiveRecord::Base.connection
     @role  = current_user.role.to_i
     @user  = current_user.username.to_s.capitalize
     @base  = "#{@user}Alkali".constantize
@@ -9,7 +9,7 @@ class DrillController < ApplicationController
     @question = Array.new()
     gon.cnt = 1
     gon.res ||= Array.new(@max, 0)
-    gon.erratum = { -1 => "danger", 0 => "default", 1 => "success" }
+    gon.erratum = { -1 => "danger", 0 => "default", 1 => "success", 2 => "warning" }
 
 
     if @role == 0 then
@@ -25,7 +25,10 @@ class DrillController < ApplicationController
     #end
 
     num = rand(1..@size)
-    @prob = @base.find(num).ent + "の" + @base.find(num).att + "は何ですか？"
+    ent = @base.find(num).ent
+    att = @base.find(num).att
+    @id = @base.find(num).id
+    @prob = ent + "の"  + att + "は何ですか？"
     @right = @base.find(num).val
     @w1 = @base.find(num%5+3).val
     @w2 = @base.find(num%5+2).val
@@ -35,6 +38,22 @@ class DrillController < ApplicationController
     @question << @w1
     @question << @w2
     @question << @w3
+
+    @is_correct = false
+    @is_wrong = false
+
+    @data = @base.find_by(id: @id)
+    #@data = @base.find_by(ent: ent, att: att, val: @right)
+    p @data
+    if @is_correct then
+      unless data.nil?
+        data.update_attribute(:state, '1')
+      end
+    elsif @is_wrong then
+      unless data.nil?
+        data.update_attribute(:state, '2')
+      end
+    end
 
   end
 end
