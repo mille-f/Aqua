23e093c3f537ee013f8359a0713850253511aad6
mille-f <f11235tetra@gmail.com>
2017-11-09 18:08:35 +0900

    [add] 視線データを元にデータベースの更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,10 +13,22 @@ class HomeController < ApplicationController
   def gaze
     data = KeywordPeriodictable.all
     if params['output'] then
+      gaze = params['gaze'].values.join.split(",").map(&:to_i)
+      #gaze.map! {|i| i+=1}
+
       data.each do |datum|
         puts "#{datum.keyword} #{datum.start} #{datum.end} #{datum.watch}"
+#=begin
+        unless gaze.empty?
+          if gaze[datum.start..datum.end].all? {|n| n > 0}
+            p gaze[datum.start..datum.end]
+            datum.update_attribute(:watch, true)
+          else
+            datum.update_attribute(:watch, false)
+          end
+        end
+#=end
       end
-      p params['gaze'].values.join.split(",").map(&:to_i)
     end
   end
 
