37707b71a880696d2cc0574e635ac9ec39e9e45d
mille-f <f11235tetra@gmail.com>
2017-11-27 20:05:15 +0900

    キーワードを出現回数で降順ソート

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,24 +12,50 @@ class HomeController < ApplicationController
 
   def gaze
     data = KeywordPeriodictable.all
+    model = Test1Periodictable.all
+
+    arr = []
+    cnt = Hash.new(0)
+
     if params['output'] then
       gaze = params['gaze'].values.join.split(",").map(&:to_i)
-      #gaze.map! {|i| i+=1}
-
       data.each do |datum|
         puts "#{datum.keyword} #{datum.start} #{datum.end} #{datum.watch}"
-#=begin
+        # startからendまで見ていればwatchをtrue
         unless gaze.empty?
           if gaze[datum.start..datum.end].all? {|n| n > 0}
-            p gaze[datum.start..datum.end]
             datum.update_attribute(:watch, true)
           else
             datum.update_attribute(:watch, false)
           end
         end
-#=end
       end
     end
+
+    data.order(:keyword).each do |datum|
+      if datum.watch # watchがtrueならば
+        #p datum
+=begin
+        p ent = Test1Periodictable.find_by(ent: datum.keyword)
+        p att = Test1Periodictable.find_by(att: datum.keyword)
+        p val = Test1Periodictable.find_by(val: datum.keyword)
+=end
+        model.each do |i|
+          if i.state == 0
+            k = datum.keyword
+            if (i.ent == k) || (i.att == k) || (i.val == k)
+              arr << k
+            end
+          end
+        end
+      end
+    end
+
+    arr.each do |e|
+      cnt[e] += 1
+    end
+    p cnt.sort_by {|k,v| -v}
+
   end
 
   def video
